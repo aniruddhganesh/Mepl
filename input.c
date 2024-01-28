0fee4128009d9b6c9d9c89d597b0c4149bee3670
Aniruddh <aniruddhganesh@protonmail.com>
2024-01-28 22:57:49 +0530

    Implemented autocompletion on tab for commands in a generic manner

--- input.c
+++ input.c
@@ -1,9 +1,29 @@
-#include <ncurses.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
+#include <ncurses.h>
+
 #include "main.h"
+#include "command.h"
 
 #define CTRL_KEY(c) ((c) & 037)
+
+static int auto_complete_guess(const char *input, void **search_arr, size_t nsize)
+{
+    int idx = -10;
+    bool first_occurance = true;
+
+    for (int i = 0; *(search_arr + i) != NULL; i += nsize/8) {
+        char *s = (char *)(*(search_arr +i));
+        if (!strncmp(input, s, strlen(input))) {
+            idx = (first_occurance) ? i/8 : -10;
+            first_occurance = false;
+        }
+    }
+
+    return idx;
+}
 
 static void input_insert_char(struct display *d, char ch)
 {
@@ -26,6 +46,30 @@ static void input_backspace(struct display *d)
     wmove(d->win_input, 0, d->input_len);
 }
 
+static void input_autocomplete(struct display *d, size_t cur_block)
+{
+    bool is_command = (strchr(d->input, ' ')) ? true : false;
+    void *search_arr = (is_command) ? NULL  : cmds;
+    size_t nsize_arr = (is_command) ? 0     : sizeof(*cmds);
+
+    // Temporary
+    if (!search_arr) return;
+
+    int byte_index = auto_complete_guess(d->input, search_arr, nsize_arr);
+    if (byte_index < 0) return;
+
+    char *autocmp_str = *((char **)search_arr + byte_index);
+    int autocmp_strlen = strlen(autocmp_str);
+
+    if (autocmp_strlen > cur_block) return;
+
+    strcat(d->input, autocmp_str + d->input_len);
+    mvwprintw(d->win_input, 0, d->input_len, "%s", autocmp_str + d->input_len);
+    free(autocmp_str);
+
+    d->input_len += (autocmp_strlen - d->input_len);
+}
+
 int freadline(struct display *d, const size_t block_len)
 {
     int ch = '\0';
@@ -40,6 +84,9 @@ int freadline(struct display *d, const size_t block_len)
                 case '\n':
                     input_insert_newline(d);
                     return d->input_len;
+                case '\t':
+                    input_autocomplete(d, cur_block);
+                    break;
                 case CTRL_KEY('w'):
                     while (d->input_len > 0) {
                         input_backspace(d);
